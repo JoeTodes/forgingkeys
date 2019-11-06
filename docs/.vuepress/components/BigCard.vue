5b416715d2e3b234c5093ab3168b5f0f3fdc2aea
Joe T. Wilson <jtwilson.home@gmail.com>
2019-11-05 21:58:07 -0700

    added favicon

--- docs/.vuepress/components/BigCard.vue
+++ docs/.vuepress/components/BigCard.vue
@@ -3,15 +3,15 @@
     <br />
     <v-container>
       <v-row>
-        <v-col cols="3"></v-col>
-        <v-col cols="6">
+        <v-col cols="1" :sm="3"></v-col>
+        <v-col cols="10" :sm="6">
           <v-card raised>
             <v-img eager :src="img"></v-img>
             <div></div>
             <v-card-subtitle v-if="caption">{{caption}}</v-card-subtitle>
           </v-card>
         </v-col>
-        <v-col cols="3"></v-col>
+        <v-col cols="1" :sm="3"></v-col>
       </v-row>
     </v-container>
     <br />
