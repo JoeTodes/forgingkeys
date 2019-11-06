8d63af0c3c543c70c95267fa10b78de1632ae26c
Joe T. Wilson <jtwilson.home@gmail.com>
2019-11-05 20:45:56 -0700

    fixed sidebard and hopefully CORS

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,7 +7,22 @@ module.exports = {
             permalink: false
         }
     },
-
+    themeConfig: {
+        displayAllHeaders: false,
+        sidebar: [
+            '/',
+            '/articles/predictions-for-worlds-collide',
+            '/articles/tips-and-tricks-for-competitive-gameplay',
+            '/articles/keyforge-for-magic-players',
+            '/articles/tempo-and-mulligans',
+            '/articles/playing-to-the-board-state',
+            '/articles/buying-yourself-another-turn',
+            "/articles/giving-your-deck-a-chance",
+            '/articles/the-hard-lock',
+            '/articles/doing-as-much-as-you-can',
+            '/articles/testing-out-the-card-viewer'
+        ]
+    },
     plugins: [
         [
             "vuepress-plugin-disqus-comment",
