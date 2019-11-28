f8f0cf4d3968b6d9718a1280c9e067c5472bc7ed
JoeTodes <jtwilson.home@gmail.com>
2019-11-28 13:21:57 -0700

    fixed big card for partial names

--- docs/.vuepress/components/BigCard.vue
+++ docs/.vuepress/components/BigCard.vue
@@ -1,51 +1,55 @@
 <template>
-  <div>
-    <br />
-    <v-container>
-      <v-row>
-        <v-col cols="1" :sm="3"></v-col>
-        <v-col cols="10" :sm="6">
-          <v-card raised>
-            <v-img eager :src="img"></v-img>
-            <div></div>
-            <v-card-subtitle v-if="caption">{{caption}}</v-card-subtitle>
-          </v-card>
-        </v-col>
-        <v-col cols="1" :sm="3"></v-col>
-      </v-row>
-    </v-container>
-    <br />
-  </div>
+    <div>
+        <br />
+        <v-container>
+            <v-row>
+                <v-col cols="1" :sm="3"></v-col>
+                <v-col cols="10" :sm="6">
+                    <v-card raised>
+                        <v-img eager :src="img"></v-img>
+                        <div></div>
+                        <v-card-subtitle v-if="caption">{{caption}}</v-card-subtitle>
+                    </v-card>
+                </v-col>
+                <v-col cols="1" :sm="3"></v-col>
+            </v-row>
+        </v-container>
+        <br />
+    </div>
 </template>
 <script>
 import data from "./assets/card_data.json";
 //TODO add AOA and WC
 export default {
-  props: ["name", "caption"],
-  data: function() {
-    return {
-      cotaData: data
-    };
-  },
-  computed: {
-    img: function() {
-      let url = "";
-      this.cotaData.forEach(card => {
-        if (card.card_title.toLowerCase() == this.name.toLowerCase()) {
-          url = card.front_image;
+    props: ["name", "caption"],
+    data: function() {
+        return {
+            cotaData: data
+        };
+    },
+    computed: {
+        img: function() {
+            let url = "";
+            this.cotaData.forEach(card => {
+                if (card.card_title.toLowerCase() == this.name.toLowerCase()) {
+                    url = card.front_image;
+                }
+            });
+            if (url.length > 0) {
+                return url;
+            } else {
+                this.cotaData.forEach(card => {
+                    if (
+                        card.card_title
+                            .toLowerCase()
+                            .includes(this.name.toLowerCase())
+                    ) {
+                        url = card.front_image;
+                    }
+                });
+            }
         }
-      });
-      if (url) {
-        return url;
-      } else {
-        this.cotaData.forEach(card => {
-          if (card.card_title.toLowerCase().includes(this.name.toLowerCase())) {
-            url = card.front_image;
-          }
-        });
-      }
     }
-  }
 };
 </script>
 
